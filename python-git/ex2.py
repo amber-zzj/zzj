b6e85c23b5d918486e9a50c8eeb0149dbe8dcdb1
amber-zzj <ocean_zhjiu@126.com>
2018-07-28 10:51:52 +0800

    homework

---
+++ python-git/ex2.py
@@ -0,0 +1,6 @@
+print "I have some chikens",5+3
+print "hens",5
+print "roosters",3
+print "Do I have more hens than roosters?",5>3
+print "DoI have no less than 2 roosters?",3>=2
+print "is it right?",11+2+8/9>6
